96066450a8c96352b6a400d570731494b554f63f
boulhoujjatmehdi <boulhoujjatmehdi@gmail.com>
2024-04-19 13:25:35 -0400

    .

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -3,12 +3,12 @@
 # start mariadb
 service mariadb start
 
+
+
 mysql -u root -e "CREATE DATABASE wordpress_db;"
 mysql -u root -e "CREATE USER 'wordpress_user'@'localhost' IDENTIFIED BY 'password';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress_db.* to wordpress_user@'localhost';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-# mysql -u root -e "USE wordpress_db"
-
 
 
 tail -f /dev/null
