05777a4772902fca7ef153a90520379ad74b1678
boulhoujjatmehdi <boulhoujjatmehdi@gmail.com>
2024-05-09 07:46:19 -0400

    just a save to change the virtual machine ;

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# start mariadb
-service mariadb start
+# Start MariaDB
+# /usr/bin/mysqld_safe &
 
+# Wait for MariaDB to start
+# sleep 10s
 
+# Initialize the database
+# /usr/bin/mysql < /tmp/create_db.sql
 
-mysql -u root -e "CREATE DATABASE wordpress_db;"
-mysql -u root -e "CREATE USER 'wordpress_user'@'localhost' IDENTIFIED BY 'password';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress_db.* to wordpress_user@'localhost';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+mkdir -p /run/mysqld/
+chmod 777 /run/mysqld/
 
+chown -R mysql:mysql /run/mysqld/
 
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+mariadbd --user=root --bootstrap < /tmp/create_db.sql
+
+mariadbd --user=root
+# Keep the container running
 tail -f /dev/null
